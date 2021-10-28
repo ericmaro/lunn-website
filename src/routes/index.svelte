f911d4579c513b85813e77cc1461c298eb024c76
dino basili <gordianbasili@gmail.com>
2021-10-28 15:15:32 +0300

    unfinished index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,187 @@
 <svelte:head>
-	<title>Lunn</title>
+	  <title>Lunn</title>
 </svelte:head>
 
 <div class="hero" style="height:640px !important; background-image: url(&quot;https://picsum.photos/id/1005/1600/1400&quot;);">
-  <div class="hero-overlay bg-opacity-60"></div> 
-  <div class="text-center hero-content text-neutral-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">
-            Hello there
-          </h1> 
-      <p class="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p> 
-      <button class="btn btn-primary">Get Started</button>
+    <div class="hero-overlay bg-opacity-60"></div> 
+    <div class="text-center hero-content text-neutral-content">
+        <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">
+                        Hello there
+                    </h1> 
+            <p class="mb-5">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p> 
+            <button class="btn btn-primary">Get Started</button>
+        </div>
     </div>
-  </div>
+</div>
+
+<div class="container mx-auto px-6 py-12">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <div>
+            <h2 class="text-lg lg:text-2xl 2xl:text-4xl font-medium uppercase">Our Solutions</h2>
+            <div class="w-16 bg-yellow-500 h-0.5 mt-1.5"></div>
+
+            <p class="mt-3 text-sm md:text-base lg:text-lg 2xl:text-xl">
+                Over the years, Financial Institution and Payment Processing Companies across Africa have trusted Lunn-Tech to provide them with excellent Payment solutions and many other consultancy Services as listed;
+            </p>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 font-poppins">
+
+            <div class="card border border-gray-100 shadow-lg">
+                <div class="card-body">
+                    <div class="card-title">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <h2 class="font-medium text-lg lg:text-2xl mb-1">ESB Gateway</h2>
+                        <a href="/" class="text-yellow-500">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card border border-gray-100 shadow-lg">
+                <div class="card-body">
+                    <div class="card-title">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <rect x="7" y="4" width="10" height="16" rx="1"></rect>
+                            <line x1="11" y1="5" x2="13" y2="5"></line>
+                            <line x1="12" y1="17" x2="12" y2="17.01"></line>
+                        </svg>
+                    </div>
+                    <div>
+                        <h2 class="font-medium text-lg lg:text-2xl mb-1">Mobile Banking</h2>
+                        <a href="/" class="text-yellow-500">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card border border-gray-100 shadow-lg">
+                <div class="card-body">
+                    <div class="card-title">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <h2 class="font-medium text-lg lg:text-2xl mb-1">Internet Banking</h2>
+                        <a href="/" class="text-yellow-500">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card border border-gray-100 shadow-lg">
+                <div class="card-body">
+                    <div class="card-title">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <line x1="3" y1="21" x2="21" y2="21"></line>
+                            <line x1="3" y1="10" x2="21" y2="10"></line>
+                            <polyline points="5 6 12 3 19 6"></polyline>
+                            <line x1="4" y1="10" x2="4" y2="21"></line>
+                            <line x1="20" y1="10" x2="20" y2="21"></line>
+                            <line x1="8" y1="14" x2="8" y2="17"></line>
+                            <line x1="12" y1="14" x2="12" y2="17"></line>
+                            <line x1="16" y1="14" x2="16" y2="17"></line>
+                        </svg>
+                    </div>
+                    <div>
+                        <h2 class="font-medium text-lg lg:text-2xl mb-1">Agency Banking</h2>
+                        <a href="/" class="text-yellow-500">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container mx-auto px-6 py-12">
+    <div>
+        <h2 class="text-lg lg:text-2xl 2xl:text-4xl font-medium uppercase">Our Services</h2>
+        <div class="w-16 bg-yellow-500 h-0.5 mt-1.5"></div>
+
+        <h4 class="text-base lg:text-xl 2xl:text-2xl font-medium capitalize mt-6">
+            We provide the best consultancy services for your business
+        </h4>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-10 mt-12">
+
+        <div class="card border border-gray-100 shadow-lg">
+            <div class="card-body">
+                <div class="card-title">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M9 12l2 2l4 -4"></path>
+                        <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3"></path>
+                    </svg>
+                </div>
+                <div>
+                    <h2 class="font-medium text-lg lg:text-2xl mb-1">System <br/> Audits</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="card border border-gray-100 shadow-lg">
+            <div class="card-body">
+                <div class="card-title">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="5" y="3" width="14" height="18" rx="1"></rect>
+                        <circle cx="12" cy="17" r="1"></circle>
+                     </svg>
+                </div>
+                <div>
+                    <h2 class="font-medium text-lg lg:text-2xl mb-1">Infrastructure Design and Implementation</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="card border border-gray-100 shadow-lg">
+            <div class="card-body">
+                <div class="card-title">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="3" y="15" width="6" height="6" rx="2"></rect>
+                        <rect x="15" y="15" width="6" height="6" rx="2"></rect>
+                        <rect x="9" y="3" width="6" height="6" rx="2"></rect>
+                        <path d="M6 15v-1a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v1"></path>
+                        <line x1="12" y1="9" x2="12" y2="12"></line>
+                    </svg>
+                </div>
+                <div>
+                    <h2 class="font-medium text-lg lg:text-2xl mb-1">Project Management</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container mx-auto px-6 py-12">
+    <div>
+        <h2 class="text-lg lg:text-2xl 2xl:text-4xl font-medium uppercase">Products</h2>
+        <div class="w-16 bg-yellow-500 h-0.5 mt-1.5"></div>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-10 mt-12">
+        <div class="space-y-6">
+            <h2 class="text-xl lg:text-3xl 2xl:text-5xl font-medium uppercas">Non Payment POS</h2>
+
+            <p class="text-gray-500 text-sm md:text-base 2xl:text-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <button class="btn px-6 rounded-sm capitalize bg-yellow-500 border-yellow-500">Find out more</button>
+        </div>
+
+        <div></div>
+    </div>
 </div>
