6c09b9bbfb57c94a70d4d929682dcbc6572327b8
dino basili <gordianbasili@gmail.com>
2021-10-29 16:20:04 +0300

    added footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -108,3 +108,69 @@
 	</div>
 </nav>
 <slot />
+
+<footer class="container mx-auto px-6 pt-24 font-josefin">
+	<div class="flex flex-wrap items-start space-y-6 lg:space-y-0 lg:space-x-6 py-12">
+		<div class="w-full lg:w-1/3 space-y-6">
+			<div>
+				<img src={logo} alt="lunn-logo" class="h-20 w-auto mb-2">
+				<p class="text-gray-500">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore rem, ipsum fugiat quaerat facere non aliquam nihil. Molestiae earum sequi, natus eum perferendis accusamus neque pariatur perspiciatis molestias, exercitationem excepturi.
+				</p>
+			</div>
+			
+			<div>
+				<p>Socials</p>
+				<div class="flex align-center space-x-4 mt-3">
+					<!-- facebook -->
+					<a href="/" class="border border-yellow-500 p-2 rounded-full text-yellow-500 cursor-pointer">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+						 </svg>
+					</a>
+
+					<!-- twitter -->
+					<a href="/" class="border border-yellow-500 p-2 rounded-full text-yellow-500 cursor-pointer">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
+						 </svg>
+					</a>
+
+					<!-- instagram -->
+					<a href="/" class="border border-yellow-500 p-2 rounded-full text-yellow-500 cursor-pointer">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+							<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+							<rect x="4" y="4" width="16" height="16" rx="4"></rect>
+							<circle cx="12" cy="12" r="3"></circle>
+							<line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
+						 </svg>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex flex-col w-full md:w-1/2 lg:w-1/5">
+			<span class="font-semibold mb-2">Location</span> 
+			<p class="text-gray-500">21 Salamander Close <br> Comma 18 Tema Ghana</p>
+		</div> 
+		<div class="flex flex-col w-full md:w-1/2 lg:w-1/5">
+			<span class="font-semibold mb-2">Contact</span> 
+			<div class="text-gray-500">
+				<p>helpdesk@lunn-tech.com</p>
+				<p>(+1) 917-801 8044</p>
+			</div>
+		</div> 
+		<div class="flex flex-col w-full md:w-1/2 lg:w-1/5">
+			<span class="font-semibold mb-2">Working Hours</span> 
+			<div class="text-gray-500">
+				<p>Weekdays 9am - 18pm</p>
+				<p>Weekend Closed</p>
+			</div>
+		</div>
+	</div>
+	<div class="border-t py-8 text-center text-gray-500">
+		<p>Copyright 2021 Lunn Tehcnologies Ltd All Rights Reserved</p>
+	</div>
+</footer>
