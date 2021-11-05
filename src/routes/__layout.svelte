181240758259a32e9a491f200a767592125692bd
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-11-05 13:34:12 +0300

    home page color update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -25,7 +25,7 @@
 
 			<div class="flex space-x-2">
 				<span>
-					<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-orange-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+					<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-orange-400" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 						<circle cx="12" cy="12" r="9"></circle>
 						<polyline points="12 7 12 12 15 15"></polyline>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,7 +19,7 @@
 }
 
 .bg-animated-button-2 {
-	background: linear-gradient(to right, white 50%, #F59E0B 50%);
+	background: linear-gradient(to right, white 50%, #FB923C 50%);
 	background-size: 200% 100%;
 	background-position: right bottom;
 	transition: all .5s ease-out;
@@ -27,7 +27,7 @@
 
 .bg-animated-button-2:hover {
 	background-position: left bottom;
-    color: #F59E0B;
+    color: #FB923C;
 }
 </style>
 
@@ -79,7 +79,7 @@
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
         <div>
             <h2 class="text-xl lg:text-2xl 2xl:text-4xl font-bold uppercase">Our Solutions</h2>
-            <div class="w-16 bg-yellow-500 h-1 mt-1.5"></div>
+            <div class="w-16 bg-orange-400 h-1 mt-1.5"></div>
 
             <p class="mt-3 text-sm md:text-base lg:text-lg xl:text-xl">
                 Over the years, Financial Institution and Payment Processing Companies across Africa have trusted Lunn-Tech to provide them with excellent Payment solutions and many other consultancy Services as listed;
@@ -91,14 +91,14 @@
             <div class="card border border-gray-100 shadow-lg">
                 <div class="card-body">
                     <div class="card-title">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                     </div>
                     <div>
                         <h2 class="font-semibold text-lg lg:text-2xl mb-1">ESB Gateway</h2>
-                        <a href="/" class="text-yellow-500 text-sm 2xl:text-base">View Details</a>
+                        <a href="/" class="text-orange-400 text-sm 2xl:text-base">View Details</a>
                     </div>
                 </div>
             </div>
@@ -106,7 +106,7 @@
             <div class="card border border-gray-100 shadow-lg">
                 <div class="card-body">
                     <div class="card-title">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <rect x="7" y="4" width="10" height="16" rx="1"></rect>
                             <line x1="11" y1="5" x2="13" y2="5"></line>
@@ -115,7 +115,7 @@
                     </div>
                     <div>
                         <h2 class="font-semibold text-lg lg:text-2xl mb-1">Mobile Banking</h2>
-                        <a href="/" class="text-yellow-500 text-sm 2xl:text-base">View Details</a>
+                        <a href="/" class="text-orange-400 text-sm 2xl:text-base">View Details</a>
                     </div>
                 </div>
             </div>
@@ -123,13 +123,13 @@
             <div class="card border border-gray-100 shadow-lg">
                 <div class="card-body">
                     <div class="card-title">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     </div>
                     <div>
                         <h2 class="font-semibold text-lg lg:text-2xl mb-1">Internet Banking</h2>
-                        <a href="/" class="text-yellow-500 text-sm 2xl:text-base">View Details</a>
+                        <a href="/" class="text-orange-400 text-sm 2xl:text-base">View Details</a>
                     </div>
                 </div>
             </div>
@@ -137,7 +137,7 @@
             <div class="card border border-gray-100 shadow-lg">
                 <div class="card-body">
                     <div class="card-title">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <line x1="3" y1="21" x2="21" y2="21"></line>
                             <line x1="3" y1="10" x2="21" y2="10"></line>
@@ -151,7 +151,7 @@
                     </div>
                     <div>
                         <h2 class="font-semibold text-lg lg:text-2xl mb-1">Agency Banking</h2>
-                        <a href="/" class="text-yellow-500 text-sm 2xl:text-base">View Details</a>
+                        <a href="/" class="text-orange-400 text-sm 2xl:text-base">View Details</a>
                     </div>
                 </div>
             </div>
@@ -183,7 +183,7 @@
 <div class="container mx-auto px-6 py-12 lg:py-24 font-josefin">
     <div>
         <h2 class="text-xl lg:text-2xl 2xl:text-4xl font-bold uppercase">Our Services</h2>
-        <div class="w-16 bg-yellow-500 h-1 mt-1.5"></div>
+        <div class="w-16 bg-orange-400 h-1 mt-1.5"></div>
 
         <h4 class="text-lg lg:text-xl font-medium capitalize mt-6">
             We provide the best consultancy services for your business
@@ -195,7 +195,7 @@
         <div class="card border border-gray-100 shadow-lg">
             <div class="card-body">
                 <div class="card-title">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M9 12l2 2l4 -4"></path>
                         <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3"></path>
@@ -210,7 +210,7 @@
         <div class="card border border-gray-100 shadow-lg">
             <div class="card-body">
                 <div class="card-title">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <rect x="5" y="3" width="14" height="18" rx="1"></rect>
                         <circle cx="12" cy="17" r="1"></circle>
@@ -225,7 +225,7 @@
         <div class="card border border-gray-100 shadow-lg">
             <div class="card-body">
                 <div class="card-title">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-yellow-500" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-orange-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <rect x="3" y="15" width="6" height="6" rx="2"></rect>
                         <rect x="15" y="15" width="6" height="6" rx="2"></rect>
@@ -245,7 +245,7 @@
 <div class="container mx-auto px-6 py-12 lg:py-24 font-josefin">
     <div>
         <h2 class="text-xl lg:text-2xl 2xl:text-4xl font-bold uppercase">Products</h2>
-        <div class="w-16 bg-yellow-500 h-1 mt-1.5"></div>
+        <div class="w-16 bg-orange-400 h-1 mt-1.5"></div>
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-10 mt-12 items-center">
@@ -256,7 +256,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
 
-            <button class="btn px-6 rounded-sm capitalize bg-yellow-500 border-yellow-500">Find out more</button>
+            <button class="btn px-6 rounded-sm capitalize bg-orange-400 border-orange-400">Find out more</button>
         </div>
 
         <div class="">
@@ -315,7 +315,7 @@
                         rows="2"></textarea>
                 </div>
     
-                <button class="btn px-6 rounded-sm capitalize border-yellow-500 bg-yellow-500">Submit</button>
+                <button class="btn px-6 rounded-sm capitalize border-orange-400 bg-orange-400">Submit</button>
             </div>
     
         </div>
@@ -333,7 +333,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
 
-           <button class="btn px-6 rounded-sm capitalize border-yellow-500 bg-yellow-500 mt-3">Find out more</button>
+           <button class="btn px-6 rounded-sm capitalize border-orange-400 bg-orange-400 mt-3">Find out more</button>
         </div>
 
         <div class="space-y-4">
@@ -345,7 +345,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
  
-            <button class="btn px-6 rounded-sm capitalize border-yellow-500 bg-yellow-500 mt-3">Find out more</button>
+            <button class="btn px-6 rounded-sm capitalize border-orange-400 bg-orange-400 mt-3">Find out more</button>
         </div>
 
         <div class="space-y-4">
@@ -357,7 +357,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
  
-            <button class="btn px-6 rounded-sm capitalize border-yellow-500 bg-yellow-500 mt-3">Find out more</button>
+            <button class="btn px-6 rounded-sm capitalize border-orange-400 bg-orange-400 mt-3">Find out more</button>
         </div>
    </div> 
 </div>
