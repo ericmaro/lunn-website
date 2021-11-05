a8c77443a9160b972fd45afc09b23510234ad9c0
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-11-05 13:47:01 +0300

    added play  button

--- src/routes/company.svelte
+++ src/routes/company.svelte
@@ -174,6 +174,16 @@
     
             <div class="relative flex justify-center">
                 <img src={companyPageImg} alt="5343" class="object-center">
+                <div class="absolute insert-0 w-full h-full">
+                    <div class="flex justify-center items-center w-full h-full">
+                        <div class="flex justify-center items-center animate-pulse bg-orange-400 h-16 w-16 rounded-2xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
             </div>
     
         </div>
