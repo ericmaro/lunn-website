0fcb80ad8d4b98e871ad0a11e495517b6d60c2e1
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-11-17 15:10:09 +0300

    added content to solution layout

--- src/routes/solutions/__layout.svelte
+++ src/routes/solutions/__layout.svelte
@@ -1,9 +1,82 @@
 
-<div class="container mx-auto px-4 flex space-x-8">
-    <div class="w-full md:w-3/5 xl:w-3/4">
-        <slot />
-    </div>
+<div class="bg-gray-50 py-12">
+    <div class="container mx-auto px-4 flex space-x-8 font-josefin">
+        <div class="bg-white w-full md:w-3/5 xl:w-3/4">
+            <slot />
+        </div>
+    
+        <div class="w-full md:w-2/5 xl:w-1/4 space-y-8">
+            <div class="border rounded-md py-6 px-8 bg-white">
+                <h2 class="text-lg md:text-xl xl:text-3xl font-bold xl mb-8">All Our Services</h2>
+    
+                <ul class="text-base xl:text-lg divide-y divide-gray-200">
 
-    <div class="w-full md:w-2/5 xl:w-1/4">
+                    <li class="flex justify-between items-center rounded-sm hover:bg-orange-400 hover:text-white px-4 py-4">
+                        <a href="/solutions/gateway/">ESB Gateway Solution</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke-width="2" stroke="#ffff" fill="#ffff" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)"></path>
+                        </svg>
+                    </li>
+
+                    <li class="flex justify-between items-center rounded-sm hover:bg-orange-400 hover:text-white px-4 py-4">
+                        <a href="/solutions/mobile_banking/">Mobile & Internet Banking</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke-width="2" stroke="#ffff" fill="#ffff" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)"></path>
+                        </svg>
+                    </li>
+
+                    <li class="flex justify-between items-center rounded-sm hover:bg-orange-400 hover:text-white px-4 py-4">
+                        <a href="/solutions/agency_banking/">Agency Banking</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke-width="2" stroke="#ffff" fill="#ffff" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)"></path>
+                        </svg>
+                    </li>
+
+                    <li class="flex justify-between items-center rounded-sm hover:bg-orange-400 hover:text-white px-4 py-4">
+                        <a href="/solutions/payment_services/">Custom Payment Services</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" stroke-width="2" stroke="#ffff" fill="#ffff" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)"></path>
+                        </svg>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="border rounded-md bg-blue-900 text-white py-6 px-8">
+                <h2 class="text-lg md:text-xl xl:text-3xl font-bold xl mb-8">Working Hours</h2>
+    
+                <ul class="text-base xl:text-lg divide-y divide-gray-200">
+
+                    <li class="px-3 py-4">
+                        <p>Monday-Friday 9:00 - 18:00 pm</p>
+                    </li>
+
+                    <li class="px-3 py-4">
+                        <p>Saturday Closed</p>
+                    </li>
+
+                    <li class="px-3 py-4">
+                        <p>Sunday Closed</p>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div>
+                <img src="https://images.pexels.com/photos/8867434/pexels-photo-8867434.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" class="border-t-md" alt="">
+                
+                <div class="bg-gray-800 text-white rounded-b-md flex space-x-4 py-6 px-8">
+                    <div></div>
+                    <div>
+                        <p>Call to ask any Question</p>
+                        <p class="text-orange-400">(+1) 917 8108044</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
