1b01a5da41d8448016d1afb17b66fa6c69066656
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-11-17 16:03:23 +0300

    added content to gateway page

--- src/routes/solutions/gateway.svelte
+++ src/routes/solutions/gateway.svelte
@@ -1,1 +1,44 @@
-<div>Gateway</div>
+<div>
+    <img src="https://images.pexels.com/photos/8475145/pexels-photo-8475145.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" class="object-cover rounded-md" alt="">
+
+    <div class="space-y-4 text-sm md:text-base xl:text-lg text-justify mt-10">
+        <p>
+            The ESB gateway enables the interface of data between 2 or more systems in a 
+            Service Oriented Architecture. Banks across the globe have swiftly embraced 
+            electronic banking across multiple channels. As banks have adopted technology 
+            for multi-channel banking, the limitation of technology to integrate with existing 
+            systems of the bank to provide reliable information has been a challenge. Lunn-Tech 
+            has developed the payBridge ESB gateway for system integration between the many 
+            disparate systems running at the bank. The payBridge ESB gateway enables the interface 
+            of data between 2 or more systems in a Service Oriented Architecture. 
+        </p>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+
+            <div class="bg-orange-100 rounded-md text-center space-y-6 py-6 px-3">
+                <h2 class="text-lg font-bold">The Core</h2>
+
+                <p>
+                    This middleware tool offers the distribution of work among connected components.
+                </p>
+            </div>
+
+            <div class="bg-orange-100 rounded-md text-center space-y-6 py-6 px-3">
+                <h2 class="text-lg font-bold">Fraud Guard</h2>
+
+                <p>
+                    This module offers an extra security layer for all transactions running through ESB Gateway.
+                </p>
+            </div>
+
+            <div class="bg-orange-100 rounded-md text-center space-y-6 py-6 px-3">
+                <h2 class="text-lg font-bold">Dashboards</h2>
+
+                <p>
+                    This module gives a visual display of the most important information on the transactions.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</div>
