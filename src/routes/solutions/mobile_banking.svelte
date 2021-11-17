6d4082718e8df123022d8fec3893c82d16d67186
Gordian Basili <55690016+digitalbunny@users.noreply.github.com>
2021-11-17 15:49:50 +0300

    Added content in mibile banking

--- src/routes/solutions/mobile_banking.svelte
+++ src/routes/solutions/mobile_banking.svelte
@@ -1,1 +1,31 @@
-<div>Mobile Banking</div>
+<div>
+    <img src="https://images.pexels.com/photos/6812431/pexels-photo-6812431.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" class="object-cover rounded-md" alt="">
+
+    <div class="space-y-4 text-sm md:text-base xl:text-lg text-justify mt-10">
+        <p>
+            Modern-day Banking Customers want access to more than one channel to transact. 
+            Our solution will be integrated seamlessly with the Core Banking System. Banking 
+            on the move Without a comprehensive solution in today’s competitive financial market, 
+            customers are bound to select an alternate financial service provider. With Lunn - Tech 
+            Internet Banking Solution, we will provide a comprehensive Internet Banking Solution. 
+            The solution will be integrated seamlessly with the Core Banking System, ATM Switch, 
+            and any other 3rd party systems e.g. Utilities and Telcos. 
+        </p>
+
+        <p>The Lunn -Tech Internet Banking Value Add;</p>
+
+        <ul class="list-inside list-disc">
+            <li>Cost-effective security components</li>
+            <li>Supports 24/7 operations and uptime</li>
+            <li>Access to balances, bill payments, account history, foreign exchange, payment of invoices, P2P payments, transaction status, Air-time, etc.</li>
+            <li>Fast implementation and integration to any CBS • Scalable support model and Improved customer service</li>
+            <li>Differentiation in the market by offering a comprehensive banking experience.</li>
+            <li>Reduced cost to serve and low integration costs</li>
+            <li>Drive new revenue streams and opportunities</li>
+            <li>Fully upgradeable service with access to Lunn-Tech support</li>
+            <li>Anytime/anywhere banking</li>
+            <li>New customers can open accounts remotely (Self Registration)</li>
+            <li>Improved service delivery.</li>
+        </ul>
+    </div>
+</div>
